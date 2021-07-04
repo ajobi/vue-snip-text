437425f728a06e4484e53f2668d82ac538d0335f
Andrej Bilec <andrej.bilec@gmail.com>
2021-07-04 20:50:38 +0200

    :wrench: Fix: Fixing the method switch from css -> js

--- src/element/element.snip.js
+++ src/element/element.snip.js
@@ -1,6 +1,9 @@
-import { snip } from 'js-snip'
+import { snip, unsnip } from 'js-snip'
 
 export const getSnipText = (state) => (el) => {
+  // TODO: add test cases for complex scenarios
+  unsnip(el)
+
   const { maxLines, snipMethod, ellipsis } = state.elementMap.get(el)
   snip(el, { maxLines, method: snipMethod, ellipsis })
 }
