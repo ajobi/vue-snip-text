05ff0888755403c3ff1c7439964ce47caa3c2778
Andrej Bilec <andrej.bilec@gmail.com>
2021-07-04 20:58:05 +0200

    :ok_hand: Improve: Removed fulltext from element state

--- src/directive/directive.inserted.js
+++ src/directive/directive.inserted.js
@@ -4,7 +4,6 @@ export const getInserted = (state, snipText) => (el, { value, arg }) => {
   const { elementMap } = state
 
   const elState = {
-    fullText: el.textContent,
     maxLines: normalizeMaxLines(state, value),
     snipMethod: normalizeSnipMethod(state, arg)
   }
