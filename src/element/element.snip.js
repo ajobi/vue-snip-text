44ecb27be29e44eef450ca52ebe9f69fee6602f4
Andrej Bilec <andrej.bilec@gmail.com>
2021-07-04 20:48:08 +0200

    :ok_hand: Improve: Removed the passed separators

--- src/element/element.snip.js
+++ src/element/element.snip.js
@@ -1,6 +1,6 @@
 import { snip } from 'js-snip'
 
 export const getSnipText = (state) => (el) => {
-  const { maxLines, snipMethod, ellipsis, separators } = state.elementMap.get(el)
-  snip(el, { maxLines, method: snipMethod, ellipsis, separators })
+  const { maxLines, snipMethod, ellipsis } = state.elementMap.get(el)
+  snip(el, { maxLines, method: snipMethod, ellipsis })
 }
